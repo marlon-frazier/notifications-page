e15bdfc243d64cda01ce2a95b338e8a765b5f7a2
Marlon <90068388+marlon-frazier@users.noreply.github.com>
2023-07-25 22:22:24 -0500

    Add files via upload

---
+++ notifications-page-main/app.js
@@ -0,0 +1,25 @@
+let count = 6
+const notifications = document.querySelector('.count')
+const user = document.querySelectorAll('.user')
+const blip = document.querySelectorAll('.blip')
+const markAll = document.querySelector('.mark-all')
+
+for (let i = 0; i < user.length; i++){
+    user[i].addEventListener('click', ()=>{
+        user[i].style.backgroundColor = 'white'
+        blip[i].classList.add('hidden')
+        notifications.innerText = count--
+    })
+}
+
+markAll.addEventListener('click', () => {
+    user.forEach( element => {
+        element.style.backgroundColor = 'white'
+    })
+
+    blip.forEach( dot => {
+        dot.classList.add('hidden')
+    })
+    notifications.innerText = 0
+})
+console.log(blip)
